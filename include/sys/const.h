3a5579719b829140c1b82ffb5f84eadac1b10bd7
jnow-87 <jan.nowotsch@gmail.com>
2016-02-14 13:51:01 +0100

    [sys header]
    	- move units.h -> const.h
    	- add further constants

---
+++ include/sys/const.h
@@ -0,0 +1,31 @@
+#ifndef SYS_UNITS_H
+#define SYS_UNITS_H
+
+
+/* numeric constants */
+#define _1k		(1 * 1024)
+#define _2k		(2 * 1024)
+#define _4k		(4 * 1024)
+#define _8k		(8 * 1024)
+#define _16k	(16 * 1024)
+#define _32k	(32 * 1024)
+#define _64k	(64 * 1024)
+#define _128k	(128 * 1024)
+#define _256k	(256 * 1024)
+#define _512k	(512 * 1024)
+#define _1M		(1 * 1024 * 1024)
+#define _2M		(2 * 1024 * 1024)
+#define _4M		(4 * 1024 * 1024)
+#define _8M		(8 * 1024 * 1024)
+#define _16M	(16 * 1024 * 1024)
+#define _32M	(32 * 1024 * 1024)
+#define _64M	(64 * 1024 * 1024)
+#define _128M	(128 * 1024 * 1024)
+#define _256M	(256 * 1024 * 1024)
+#define _512M	(512 * 1024 * 1024)
+#define _1G		(1 * 1024 * 1024 * 1024)
+#define _2G		(2 * 1024 * 1024 * 1024)
+#define _4G		(4 * 1024 * 1024 * 1024)
+
+
+#endif // SYS_UNITS_H
