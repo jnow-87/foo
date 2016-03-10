19dbc3b91db19a59250174b647fb351a998e8fac
jnow-87 <jan.nowotsch@gmail.com>
2016-03-10 18:46:43 +0100

    [buildsys: Kconfig]
    	- rename CONFIG_HAS_FLOAT to CONFIG_NOFLOAT and move it from
    	  sys/Kconfig to arch/Kconfig
    	- rename CONFIG_SYS_SMP to CONFIG_KERNEL_SMP and move it from
    	  sys/Kconfig to kernel/Kconfig
    	- adapt related source files and Makefiles

--- include/sys/math.h
+++ include/sys/math.h
@@ -17,13 +17,13 @@ unsigned int log(unsigned int x, unsigned long base);
 unsigned long long pow(unsigned long long x, unsigned long long y);
 unsigned int powi(unsigned int x, unsigned int y);
 
-#ifdef CONFIG_HAS_FLOAT
+#if !defined(CONFIG_NOFLOAT) || CONFIG_NOFLOAT == 0
 
 double powd(double x, double y);
 float powf(float x, float y);
 long double powl(long double x, long double y);
 
-#endif // CONFIG_HAS_FLOAT
+#endif // CONFIG_NOFLOAT
 
 
 #endif // SYS_MATH_H
