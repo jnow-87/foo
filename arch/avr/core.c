f6b88f5c284be8ac4d8d881e00c8f5b0929f28f9
jnow-87 <jan.nowotsch@gmail.com>
2017-05-25 09:57:26 +0200

    [avr: sleep mode]
    	- add sleep mode selection for avr_core_sleep()

--- arch/avr/core.c
+++ arch/avr/core.c
@@ -5,7 +5,30 @@
 
 /* global functions */
 void avr_core_sleep(void){
+	/* set sleep mode */
+	mreg_w(SMCR, (0x1 << SMCR_SE) |
+#if CONFIG_AVR_SLEEPMODE_IDLE
+		(0x0 << SMCR_SM)
+#elif CONFIG_AVR_SLEEPMODE_ADCNR
+		(0x1 << SMCR_SM)
+#elif CONFIG_AVR_SLEEPMODE_PWRDWN
+		(0x2 << SMCR_SM)
+#elif CONFIG_AVR_SLEEPMODE_PWRSAVE
+		(0x3 << SMCR_SM)
+#elif CONFIG_AVR_SLEEPMODE_STANDBY
+		(0x6 << SMCR_SM)
+#elif CONFIG_AVR_SLEEPMODE_EXTSTANDBY
+		(0x7 << SMCR_SM)
+#else
+	#error "invalid sleep mode, check kernel config"
+#endif // CONFIG_AVR_SLEEPMODE
+	);
+
+	/* send core to sleep */
 	asm volatile("sleep");
+
+	/* prevent accidental sleep */
+	mreg_w(SMCR, 0x0);
 }
 
 void avr_core_halt(void){
