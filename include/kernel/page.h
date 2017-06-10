2f73fedbaee933913c65841a077457815d05af92
jnow-87 <jan.nowotsch@gmail.com>
2017-06-10 10:00:29 +0200

    [kernel: memory management page control]
    	- remove differentiation of page handling for targets with and
    	  without virtual memory
    
    	issue: 12

--- include/kernel/page.h
+++ include/kernel/page.h
@@ -3,17 +3,93 @@
 
 
 #include <config/config.h>
+#include <kernel/process.h>
 
 
-#if defined(CONFIG_KERNEL_VIRT_MEM)
+/* macros */
+#define PAGESIZE_BYTES(psize)	((0x1 << (2 * psize)) * 4096)
 
-#include <kernel/page_virt.h>
 
-#else // CONFIG_KERNEL_VIRT_MEM
+/* incomplete types */
+struct process_t;
 
-#include <kernel/page_phys.h>
+
+/* types */
+#ifdef CONFIG_KERNEL_VIRT_MEM
+
+typedef enum{
+	PAGE_R = 0x1,
+	PAGE_W = 0x2,
+	PAGE_X = 0x4
+} page_perm_t;
+
+typedef enum{
+	PAGE_KERNEL = 0,
+	PAGE_USER
+} page_priv_t;
+
+typedef enum{
+	PAGE_COHERENT = 0x1,
+	PAGE_NOREORDER = 0x2,
+	PAGE_NOCACHE = 0x4,
+	PAGE_INVAL_PROT = 0x8,
+	PAGE_WRITE_THROUGH = 0x10
+} page_flags_t;
+
+typedef enum{
+	PAGESIZE_MIN = 0x0,
+	PAGESIZE_4k = 0x0,
+	PAGESIZE_16k,
+	PAGESIZE_64k,
+	PAGESIZE_256k,
+	PAGESIZE_1M,
+	PAGESIZE_4M,
+	PAGESIZE_16M,
+	PAGESIZE_64M,
+	PAGESIZE_256M,
+	PAGESIZE_1G,
+	PAGESIZE_MAX = PAGESIZE_1G
+} page_size_t;
 
 #endif // CONFIG_KERNEL_VIRT_MEM
+
+#ifdef CONFIG_KERNEL_NO_VIRT_MEM
+
+typedef size_t page_size_t;
+
+#endif // CONFIG_KERNEL_NO_VIRT_MEM
+
+typedef struct page_t{
+	/* mapping */
+	void *phys_addr;							// physical address
+
+#ifdef CONFIG_KERNEL_VIRT_MEM
+	void *virt_addr;							// virtual address
+
+	page_size_t psize;							// page size
+#endif // CONFIG_KERNEL_VIRT_MEM
+
+	/* flags */
+#ifdef CONFIG_KERNEL_VIRT_MEM
+	page_flags_t flags;							// other flags
+	page_priv_t priv;							// privilege level
+	page_perm_t perm;							// permissions
+#endif // CONFIG_KERNEL_VIRT_MEM
+
+	/* index */
+#ifdef CONFIG_KERNEL_VIRT_MEM
+	unsigned int idx;							// index in page table/TLB
+#endif // CONFIG_KERNEL_VIRT_MEM
+
+	/* list handling */
+	struct page_t *prev,
+				  *next;
+} page_t;
+
+
+/* prototypes */
+page_t *page_alloc(struct process_t *this_p, page_size_t psize);
+void page_free(struct process_t *this_p, page_t *page);
 
 
 #endif // KERNEL_PAGE_H
--- kernel/page.c
+++ kernel/page.c
@@ -1,8 +1,10 @@
-#include <kernel/page.h>
+#include <config/config.h>
 #include <kernel/process.h>
+#include <kernel/page.h>
 #include <kernel/kmem.h>
 #include <kernel/umem.h>
 #include <sys/memblock.h>
+#include <sys/errno.h>
 
 
 /* global functions */
@@ -15,36 +17,49 @@ page_t *page_alloc(struct process_t *this_p, page_size_t psize){
 	page = kmalloc(sizeof(page_t));
 
 	if(page == 0)
-		goto err_0;
+		goto_errno(err_0, E_NOMEM);
+
+#if defined(CONFIG_KERNEL_VIRT_MEM)
+	if(psize < PAGESIZE_MIN || psize > PAGESIZE_MAX)
+		goto_errno(err_0, E_INVAL);
 
 	size = PAGESIZE_BYTES(psize);
+#else // CONFIG_KERNEL_VIRT_MEM
+	size = psize;
+#endif // CONFIG_KERNEL_VIRT_MEM
 
 	/* set page attributes */
+#ifdef CONFIG_KERNEL_VIRT_MEM
 	page->psize = psize;
 	page->flags = PAGE_COHERENT;
 	page->priv = PAGE_USER;
 	page->perm = PAGE_R | PAGE_W | PAGE_X;
 	page->idx = 0;
+#endif // CONFIG_KERNEL_VIRT_MEM
 
 	/* allocate physical block */
 	page->phys_addr = umalloc(size);
 
 	if(page->phys_addr == 0)
-		goto err_1;
+		goto_errno(err_1, E_NOMEM);
 
 	/* allocate virtual block */
+#ifdef CONFIG_KERNEL_VIRT_MEM
 	mutex_lock(&(this_p->memory.mtx));
 	page->virt_addr = memblock_alloc(&(this_p->memory.addr_space), size);
 	mutex_unlock(&(this_p->memory.mtx));
 
 	if(page->virt_addr == 0)
-		goto err_2;
+		goto_errno(err_2, E_NOMEM);
+#endif // CONFIG_KERNEL_VIRT_MEM
 
 	return page;
 
 
+#ifdef CONFIG_KERNEL_VIRT_MEM
 err_2:
 	ufree(page->phys_addr);
+#endif // CONFIG_KERNEL_VIRT_MEM
 
 err_1:
 	kfree(page);
@@ -55,9 +70,11 @@ err_0:
 
 void page_free(struct process_t *this_p, page_t *page){
 	/* free virtual block */
+#ifdef CONFIG_KERNEL_VIRT_MEM
 	mutex_lock(&(this_p->memory.mtx));
 	memblock_free(&(this_p->memory_space), page->virt_addr);
 	mutex_unlock(&(this_p->memory.mtx));
+#endif // CONFIG_KERNEL_VIRT_MEM
 
 	/* free physica block */
 	ufree(page->phys_addr);
