0638b2ab0c8ded976b6ecc1fcbd4ca185fd6a6eb
jnow-87 <jan.nowotsch@gmail.com>
2017-08-14 20:44:29 +0200

    [fix: process creation]
    	description: unable to create a process with starting address is 0x0
    	reason: a process' entry is explicitly checked for 0x0 and returned
    			as an error
    
    	fix: remove the check, since it is not explicitly required

--- kernel/process.c
+++ kernel/process.c
@@ -78,9 +78,6 @@ process_t *process_create(void *binary, bin_type_t bin_type, char const *name, c
 	if(bin_load(binary, bin_type, this_p, &entry) != E_OK)
 		goto err_3;
 
-	if(entry == 0x0)
-		goto err_3;
-
 	/* create first thread */
 	this_t = thread_create(this_p, 0, entry, (void*)args);
 
