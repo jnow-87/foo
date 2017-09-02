d1652050197b7302957e8070e3146d7a9f65f800
jnow-87 <jan.nowotsch@gmail.com>
2017-09-02 10:05:44 +0200

    [arch: interface - copy_to/from_user]
    	- update macros copy_to/from_user() to use memcpy() if the target
    	  architecture does not provide an implementation

--- include/arch/mem.h
+++ include/arch/mem.h
@@ -3,7 +3,9 @@
 
 
 #include <arch/arch.h>
+#include <kernel/kmem.h>
 #include <sys/errno.h>
+#include <sys/string.h>
 
 
 /* macros */
@@ -11,8 +13,11 @@
 #define page_entry_inval_idx(idx, sync)		(arch_kernel_call(page_entry_inval_idx, E_OK)(idx, sync))
 #define page_entry_inval_va(va, sync)		(arch_kernel_call(page_entry_inval_va, E_OK)(va, sync))
 #define page_entry_search(p, r)				(arch_kernel_call(page_entry_search, E_OK)(p, r))
-#define copy_from_user(tgt, src, size, p)	(arch_kernel_call(copy_from_user, E_OK)(tgt, src, size, p))
-#define copy_to_user(tgt, src, size, p)		(arch_kernel_call(copy_to_user, E_OK)(tgt, src, size, p))
+#define copy_from_user(tgt, src, size, p) \
+	(arch_cbs_kernel.copy_from_user != 0x0 ? arch_cbs_kernel.copy_from_user(tgt, src, size, p) : (memcpy(tgt, src, size), E_OK))
+
+#define copy_to_user(tgt, src, size, p)	\
+	(arch_cbs_kernel.copy_to_user != 0x0 ? arch_cbs_kernel.copy_to_user(tgt, src, size, p) : (memcpy(tgt, src, size), E_OK))
 
 
 #endif // ARCH_MEM_H
