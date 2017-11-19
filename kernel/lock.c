a4586171594159b73778c05dc6c7a5bbb2ec434b
jnow-87 <jan.nowotsch@gmail.com>
2017-11-19 09:26:59 +0100

    [fix: kernel lock]
    	issue:
    		nested calls to klock() will overwrite the stored interrupt
    		mask, hence kunlock() will not re-enable interrupts.
    
    	fix:
    		introduce a nesting counter to check if klock() is nested and
    		prevent overwriting the interrupt mask
    
    	issue: 67

--- kernel/lock.c
+++ kernel/lock.c
@@ -1,6 +1,7 @@
 #include <config/config.h>
 #include <arch/interrupt.h>
 #include <kernel/kprintf.h>
+#include <sys/types.h>
 
 #ifdef CONFIG_KERNEL_SMP
 #include <sys/mutex.h>
@@ -8,7 +9,8 @@
 
 
 /* static variables */
-static int_type_t imask;
+static volatile int_type_t imask = INT_NONE;
+static volatile uint8_t nest = 0;
 
 #ifdef CONFIG_KERNEL_SMP
 static mutex_t kmutex = NESTED_MUTEX_INITIALISER();
@@ -21,8 +23,11 @@ void klock(){
 	mutex_lock_nested(&kmutex);
 #endif // CONFIG_KERNEL_SMP
 
-	imask = int_enabled();
+	if(nest == 0)
+		imask = int_enabled();
+
 	int_enable(INT_NONE);
+	nest++;
 
 	DEBUG("\033[31mlock kernel\033[0m\n");
 }
@@ -30,7 +35,10 @@ void klock(){
 void kunlock(){
 	DEBUG("\033[32munlock kernel\033[0m\n");
 
-	int_enable(imask);
+	nest--;
+
+	if(nest == 0)
+		int_enable(imask);
 
 #ifdef CONFIG_KERNEL_SMP
 	mutex_unlock_nested(&kmutex);
