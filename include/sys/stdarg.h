c7735fc5498cb973d026a705f3593c8aa38ef078
jnow-87 <jan.nowotsch@gmail.com>
2018-02-03 06:30:28 +0100

    [fix: variadic function family for avr]
    	issue:
    		After refactoring vfprintf() strings were not printed correctly
    		for the avr architecture. Specifically, not all arguments that
    		of the variadic arguments are not printed.
    
    	reason:
    		va_list is defined as single element array on x86 machines
    		allowing va_lists to be passed between functions with all
    		functions receiving an update if either of them removes
    		arguments from the the list. On avr va_list seems to be simple
    		void pointer. Hence, if a called function removes an argument
    		from the list calling functions do not receive an update of the
    		list.
    
    	fix:
    		redefine va_list and the respective macros for the avr
    		architecture to also use a single element array.
    
    	issue: 81

--- include/sys/stdarg.h
+++ include/sys/stdarg.h
@@ -2,15 +2,40 @@
 #define SYS_STDARG_H
 
 
-/* macros */
-#define va_start(v,l)	__builtin_va_start(v,l)
-#define va_end(v)		__builtin_va_end(v)
-#define va_arg(v,l)		__builtin_va_arg(v,l)
-#define va_copy(d,s)	__builtin_va_copy(d,s)
+#include <config/config.h>
 
+
+/* macros */
+#ifdef CONFIG_AVR
+
+#define va_start(ap, last)	__builtin_va_start(ap[0], last)
+#define va_end(ap)			__builtin_va_end(ap[0])
+#define va_arg(ap, type)	__builtin_va_arg(ap[0], type)
+#define va_copy(dest, src)	__builtin_va_copy(dest[0], src[0])
+
+#else
+
+#define va_start(ap, last)	__builtin_va_start(ap, last)
+#define va_end(ap)			__builtin_va_end(ap)
+#define va_arg(ap, type)	__builtin_va_arg(ap, type)
+#define va_copy(dest, src)	__builtin_va_copy(dest, src)
+
+#endif // CONFIG_AVR
 
 /* types */
+#ifdef CONFIG_AVR
+
+// NOTE __builtin_va_list seems to be defined as void pointer. To allow
+// 		va_lists being passed between functions with all functions being
+// 		affected by updates to the list va_list is defined as an array
+// 		type as it is for x86 systems.
+typedef __builtin_va_list va_list[1];
+
+#else
+
 typedef __builtin_va_list va_list;
+
+#endif // CONFIG_AVR
 
 
 #endif // SYS_STDARG_H
