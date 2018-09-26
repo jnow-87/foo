aa46751b059fe46677dee7fb44f6633f3108f9ef
jnow-87 <jan.nowotsch@gmail.com>
2018-09-26 06:10:36 +0200

    [init: help]
    	- add init 'help' command implementation
    
    	issue: 62

--- init/cmd/help.c
+++ init/cmd/help.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
+#include <cmd/cmd.hash.h>
+
+
+/* macros */
+#define MAX_LINE_LEN	80
 
 
 /* global functions */
 int help(int argc, char **argv){
-	printf("help dummy\n");
+	size_t i,
+		   line_len,
+		   len;
+	cmd_t const *cmd;
+
+
+
+	line_len = 0;
+
+	for(i=MIN_HASH_VALUE; i<=MAX_HASH_VALUE; ++i){
+		cmd = wordlist + i;
+
+		if(cmd->name != 0 && cmd->name[0] != '\0' && cmd->exec != 0x0){
+			len = strlen(cmd->name) + 1;
+
+			if(line_len + len > MAX_LINE_LEN){
+				printf("\n");
+				line_len = 0;
+			}
+
+			line_len += len;
+
+			printf("%s\t", cmd->name);
+		}
+	}
+
+	printf("\n");
+
 	return 0;
 }
