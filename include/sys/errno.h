815d711dfeb624b44f21a6dfc36d03bbd522ff98
jnow-87 <jan.nowotsch@gmail.com>
2018-12-31 16:05:31 +0100

    [redefine errno]
    	- update errno to only contain the last error
    	- add E_UNKNOWN

--- include/sys/errno.h
+++ include/sys/errno.h
@@ -12,21 +12,22 @@
 
 
 /* macros */
-#define return_errno(e_code)		{ errno |= e_code; return (-e_code); }
-#define goto_errno(label, e_code)	{ errno |= e_code; goto label; }
+#define return_errno(e_code)		{ errno = e_code; return (-e_code); }
+#define goto_errno(label, e_code)	{ errno = e_code; goto label; }
 
 
 /* types */
 typedef enum{
-	E_OK = 0,			/**< all ok */
-	E_INVAL = 0x1,		/**< invalid argument, e.g. out of range or entry not found */
-	E_NOMEM = 0x2,		/**< out of memory */
-	E_LIMIT = 0x4,		/**< implementation limit reached */
-	E_IO = 0x8,			/**< I/O error */
-	E_NOIMP = 0x10,		/**< function not implemented */
-	E_INUSE = 0x20,		/**< resource in use */
-	E_UNAVAIL = 0x40,	/**< resource is not available */
-	E_END = 0x80,		/**< end of resource reached */
+	E_OK = 0,	/**< all ok */
+	E_INVAL,	/**< invalid argument, e.g. out of range or entry not found */
+	E_NOMEM,	/**< out of memory */
+	E_LIMIT,	/**< implementation limit reached */
+	E_IO,		/**< I/O error */
+	E_NOIMP,	/**< function not implemented */
+	E_INUSE,	/**< resource in use */
+	E_UNAVAIL,	/**< resource is not available */
+	E_END,		/**< end of resource reached */
+	E_UNKNOWN,	/**< unknown error */
 } errno_t;
 
 
