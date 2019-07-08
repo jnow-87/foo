f481217ffaf1964c5f4a8cd9fe4467cdcefcd139
jnow-87 <jan.nowotsch@gmail.com>
2019-07-08 18:05:12 +0200

    [apply device tree to main source]
    	- apply the device tree to the main source
    
    	issue: 175

--- kernel/sched/thread.c
+++ kernel/sched/thread.c
@@ -11,6 +11,7 @@
 #include <arch/thread.h>
 #include <kernel/thread.h>
 #include <kernel/memory.h>
+#include <sys/devtree.h>
 #include <sys/thread.h>
 #include <sys/list.h>
 #include <sys/stack.h>
@@ -22,6 +23,7 @@ thread_t *thread_create(struct process_t *this_p, tid_t tid, thread_entry_t entr
 	thread_t *this_t;
 	thread_ctx_t *ctx;
 	void *user_entry;
+	devtree_memory_t const *stack;
 
 
 	this_t = kmalloc(sizeof(thread_t));
@@ -43,7 +45,10 @@ thread_t *thread_create(struct process_t *this_p, tid_t tid, thread_entry_t entr
 	this_t->priority = 0;
 
 	/* prepare stack */
-	this_t->stack = page_alloc(this_p, CONFIG_KERNEL_STACK_SIZE);
+	// NOTE the memory stack is ensured to exist by the build system
+	stack = devtree_find_memory_by_name(&__dt_memory_root, "kernel_stack");
+
+	this_t->stack = page_alloc(this_p, stack->size);
 
 	if(this_t->stack == 0x0)
 		goto_errno(err_1, E_NOMEM);
@@ -57,7 +62,7 @@ thread_t *thread_create(struct process_t *this_p, tid_t tid, thread_entry_t entr
 	this_t->signal_ctx_stack = 0x0;
 	this_t->ctx_stack = 0x0;
 
-	ctx = (thread_ctx_t*)(this_t->stack->phys_addr + CONFIG_KERNEL_STACK_SIZE - sizeof(thread_ctx_t));
+	ctx = (thread_ctx_t*)(this_t->stack->phys_addr + stack->size - sizeof(thread_ctx_t));
 	thread_context_init(ctx, this_t, user_entry, entry, thread_arg);
 
 	stack_push(this_t->ctx_stack, ctx);
