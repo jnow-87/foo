f9ebb74d6c1666bbaf2b0e1be4d98efa1af5b80e
jnow-87 <jan.nowotsch@gmail.com>
2019-07-27 09:27:39 +0200

    [kernel task: remove list of queues]
    	- no longer keep track of all dependency queues, instead check for a
    	  valid queue within each task

--- include/kernel/task.h
+++ include/kernel/task.h
@@ -30,7 +30,7 @@ typedef struct ktask_t{
 	struct ktask_t *prev,
 				   *next;
 
-	struct ktask_queue_t *queue;
+	struct ktask_queue_t *dep_queue;
 	struct ktask_t *queue_next;
 
 	ktask_flag_t flags;
@@ -40,16 +40,13 @@ typedef struct ktask_t{
 } ktask_t;
 
 typedef struct ktask_queue_t{
-	struct ktask_queue_t *prev,
-						 *next;
-
 	ktask_t * volatile head,
 			* volatile tail;
 } ktask_queue_t;
 
 
 /* prototypes */
-int ktask_create(ktask_hdlr_t hdlr, void *data, size_t size, ktask_queue_t *queue, bool recurring);
+int ktask_create(ktask_hdlr_t hdlr, void *data, size_t size, ktask_queue_t *dep_queue, bool recurring);
 void ktask_complete(ktask_t *task);
 ktask_t *ktask_next(void);
 
