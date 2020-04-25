051bf85b0525e0ef52dc44bcd4b5584e6a14907a
jnow-87 <jan.nowotsch@gmail.com>
2020-04-25 18:42:48 +0200

    [ksignal update]
    	- add ksignal convenience functions that unlock/lock a given mutex
    	  or critical section lock
    	- Change the ksignal mechanism to handle signals that are send while
    	  no one is waiting for them. Instead of updating a counter, only a
    	  single occurrence is memorised
    
    	issue: 192 194 195 202

--- kernel/ksignal.c
+++ kernel/ksignal.c
@@ -13,6 +13,7 @@
 #include <kernel/memory.h>
 #include <kernel/critsec.h>
 #include <sys/queue.h>
+#include <sys/mutex.h>
 #include <sys/errno.h>
 
 
@@ -22,16 +23,11 @@ static critsec_lock_t ksignal_lock = CRITSEC_INITIALISER();
 
 /* global functions */
 void ksignal_init(ksignal_t *sig){
-	sig->unmatched = 0;
 	sig->head = 0x0;
 	sig->tail = 0x0;
+	sig->interim = false;
 }
 
-/**
- * \pre		no kernel global mutexes, such as the scheduler
- * 			mutex must be locked, active locks on a dedicated
- * 			object such	as a file descriptor are fine
- */
 void ksignal_wait(ksignal_t *sig){
 	ksignal_queue_t e;
 
@@ -40,19 +36,30 @@ void ksignal_wait(ksignal_t *sig){
 
 	critsec_lock(&ksignal_lock);
 
-	if(sig->unmatched){
-		sig->unmatched--;
-		csection_unlock(&sig_mtx);
+	if(!sig->interim){
+		queue_enqueue(sig->head, sig->tail, &e);
+		sched_thread_pause((thread_t*)e.thread);
 
-		return;
+		critsec_unlock(&ksignal_lock);
+
+		sched_yield();
 	}
+	else{
+		sig->interim = false;
+		critsec_unlock(&ksignal_lock);
+	}
+}
 
-	queue_enqueue(sig->head, sig->tail, &e);
-	sched_thread_pause((thread_t*)e.thread);
+void ksignal_wait_mtx(ksignal_t *sig, mutex_t *mtx){
+	mutex_unlock(mtx);
+	ksignal_wait(sig);
+	mutex_lock(mtx);
+}
 
-	critsec_unlock(&sig_mtx);
-
-	sched_yield();
+void ksignal_wait_crit(ksignal_t *sig, critsec_lock_t *lock){
+	critsec_unlock(lock);
+	ksignal_wait(sig);
+	critsec_lock(lock);
 }
 
 void ksignal_send(ksignal_t *sig){
@@ -64,7 +71,7 @@ void ksignal_send(ksignal_t *sig){
 	e = queue_dequeue(sig->head, sig->tail);
 
 	if(e != 0x0)	sched_thread_wake((thread_t*)e->thread);
-	else			sig->unmatched++;
+	else			sig->interim = true;
 
 	critsec_unlock(&ksignal_lock);
 }
@@ -76,7 +83,7 @@ void ksignal_bcast(ksignal_t *sig){
 	critsec_lock(&ksignal_lock);
 
 	if(list_empty(sig->head))
-		sig->unmatched++;
+		sig->interim = true;
 
 	while(1){
 		e = queue_dequeue(sig->head, sig->tail);
