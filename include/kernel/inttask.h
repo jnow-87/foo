3c1fc88322b3598aa0c91e689fa05c6eaa1532ec
jnow-87 <jan.nowotsch@gmail.com>
2020-04-25 18:49:36 +0200

    [fix inttask locking]
    	issue:
    		System lock while trying to lock a mutex within itask_complete()
    
    	reason:
    		The inttask implementation relied on mutexes for locking.
    		However, if an interrupt calls itask_complete() while interrupts
    		are disabled and the respective mutex is locked, this would lock
    		the system since the itask_complete() is unable to acquire the
    		mutex and the mutex cannot be released since interrupts are
    		disabled.
    
    	fix:
    		Update the inttask implementation to use critical section locks
    		instead of mutexes
    
    	issue: 195 202

--- include/kernel/inttask.h
+++ include/kernel/inttask.h
@@ -13,8 +13,8 @@
 
 #include <arch/interrupt.h>
 #include <kernel/ksignal.h>
+#include <kernel/critsec.h>
 #include <sys/types.h>
-#include <sys/mutex.h>
 
 
 /* types */
@@ -29,7 +29,7 @@ typedef struct{
 	itask_t *head,
 			*tail;
 
-	mutex_t mtx;
+	critsec_lock_t lock;
 } itask_queue_t;
 
 
