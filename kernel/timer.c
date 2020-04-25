a4b31b42ac51296f40249200603b6586ee83872f
jnow-87 <jan.nowotsch@gmail.com>
2020-04-25 18:57:05 +0200

    [fix kernel timer]
    	issue:
    		unprotected concurrent access to list and global time
    
    	reason:
    		The kernel timer uses a list to keep track of all active timers
    		and a gobal variable for the system time. Both are accesses from
    		within the respective syscalls and the timer interrupt. However,
    		those accesses are not protected at all.
    
    	fix:
    		add a global timer critical section lock
    
    	issue: 195 202

--- kernel/timer.c
+++ kernel/timer.c
@@ -12,6 +12,7 @@
 #include <kernel/memory.h>
 #include <kernel/syscall.h>
 #include <kernel/ksignal.h>
+#include <kernel/critsec.h>
 #include <sys/types.h>
 #include <sys/list.h>
 
@@ -35,11 +36,12 @@ static int sc_hdlr_sleep(void *p);
 static int sc_hdlr_time(void *p);
 
 static size_t to_ticks(uint32_t us);
-static void time_carry(void);
+static void update_time(void);
 
 
 /* static variables */
 static timer_t *timer_lst = 0x0;
+static critsec_lock_t timer_lock = CRITSEC_INITIALISER();
 static uint32_t time_us = 0;
 static time_t time = { 0 };
 
@@ -53,15 +55,19 @@ void ktimer_tick(void){
 	time_us += CYCLE_TIME_US;
 
 	if(time_us + CYCLE_TIME_US < time_us)
-		time_carry();
+		update_time();
 
 	/* update timer */
+	critsec_lock(&timer_lock);
+
 	list_for_each(timer_lst, t){
 		t->ticks--;
 
 		if(t->ticks == 0)
 			ksignal_send(&t->sig);
 	}
+
+	critsec_unlock(&timer_lock);
 }
 
 
@@ -81,41 +87,34 @@ static int init(void){
 kernel_init(0, init);
 
 static int sc_hdlr_sleep(void *_p){
-	timer_t *t;
+	size_t ticks;
+	timer_t t;
 	time_t *p;
 
 
 	p = (time_t*)_p;
 
-	/* allocate timer */
-	t = kmalloc(sizeof(timer_t));
+	/* compute ticks */
+	if(p->us)		ticks = to_ticks(p->us);
+	else if(p->ms)	ticks = to_ticks((uint32_t)p->ms * 1000);
+	else			return_errno(E_INVAL);
 
-	if(t == 0x0)
-		return -errno;
+	if(ticks == 0)
+		return_errno(E_LIMIT);
 
 	/* init timer */
-	ksignal_init(&t->sig);
+	t.ticks = ticks;
+	ksignal_init(&t.sig);
 
-	if(p->us)		t->ticks = to_ticks(p->us);
-	else if(p->ms)	t->ticks = to_ticks((uint32_t)p->ms * 1000);
-	else			goto_errno(clean, E_INVAL);
+	critsec_lock(&timer_lock);
 
-	if(t->ticks == 0)
-		goto_errno(clean, E_LIMIT);
+	list_add_tail(timer_lst, &t);
+	ksignal_wait_crit(&t.sig, &timer_lock);
+	list_rm(timer_lst, &t);
 
-	list_add_tail(timer_lst, t);
+	critsec_unlock(&timer_lock);
 
-	/* wait for timer to expire */
-	ksignal_wait(&t->sig);
-
-	/* cleanup */
-	list_rm(timer_lst, t);
-
-
-clean:
-	kfree(t);
-
-	return -errno;
+	return E_OK;
 }
 
 static int sc_hdlr_time(void *_p){
@@ -124,7 +123,7 @@ static int sc_hdlr_time(void *_p){
 
 	p = (time_t*)_p;
 
-	time_carry();
+	update_time();
 	*p = time;
 
 	return E_OK;
@@ -142,9 +141,11 @@ static size_t to_ticks(uint32_t us){
 	return ticks;
 }
 
-static void time_carry(void){
+static void update_time(void){
 	uint32_t x;
 
+
+	critsec_lock(&timer_lock);
 
 	/* convert seconds */
 	x = time_us / 1000000;
@@ -162,4 +163,6 @@ static void time_carry(void){
 	time.us += time_us;
 
 	time_us = 0;
+
+	critsec_unlock(&timer_lock);
 }
