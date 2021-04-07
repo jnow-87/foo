2bb9d7aa311d4504a8918ea5e0ba4f824760e737
jnow-87 <jan.nowotsch@gmail.com>
2021-04-07 13:51:00 +0200

    [x86 uart driver interrupts]
    - enable interrupt support for the x86 uart driver
    - update the x86 uart driver to use the integration test framework
      HWO_UART_CFG hardware operation
    - update the x86 device tree accordingly
    
    issue: 225

--- driver/uart/x86.c
+++ driver/uart/x86.c
@@ -8,12 +8,26 @@
 
 
 #include <arch/x86/linux.h>
+#include <arch/x86/hardware.h>
 #include <kernel/memory.h>
 #include <kernel/driver.h>
 #include <driver/term.h>
 #include <sys/types.h>
+#include <sys/string.h>
 #include <sys/errno.h>
 #include <sys/uart.h>
+
+
+/* types */
+typedef struct{
+	char const *path;
+	uint8_t const rx_int;
+} dt_data_t;
+
+typedef struct{
+	dt_data_t *dtd;
+	int fd;
+} dev_data_t;
 
 
 /* local/static prototypes */
@@ -26,30 +40,60 @@ static size_t gets(char *s, size_t n, term_err_t *err, void *data);
 
 /* local functions */
 static void *probe(char const *name, void *dt_data, void *dt_itf){
+	dt_data_t *dtd;
 	term_itf_t *itf;
+	dev_data_t *uart;
 
 
+	dtd = (dt_data_t*)dt_data;
 	itf = kmalloc(sizeof(term_itf_t));
+	uart = kmalloc(sizeof(dev_data_t));
 
-	if(itf == 0x0)
-		return 0x0;
+	if(itf == 0x0 || uart == 0x0)
+		goto err;
+
+	uart->dtd = dtd;
+	uart->fd = lnx_open(dtd->path, LNX_O_RDWR, 0666);
 
 	itf->configure = configure;
 	itf->get_flags = get_flags;
 	itf->putc = putc;
 	itf->puts = putsn;
 	itf->gets = gets;
-	itf->data = 0x0;
-	itf->rx_int = 0;
+	itf->data = uart;
+	itf->rx_int = dtd->rx_int;
 	itf->tx_int = 0;
 	itf->cfg_size = sizeof(uart_cfg_t);
 
 	return itf;
+
+
+err:
+	kfree(uart);
+	kfree(itf);
+
+	return 0x0;
 }
 
 interface_probe("x86,uart", probe);
 
-static int configure(void *_cfg, void *data){
+static int configure(void *cfg, void *data){
+	dev_data_t *uart;
+	x86_hw_op_t op;
+
+
+	uart = (dev_data_t*)data;
+
+	op.num = HWO_UART_CFG;
+
+	op.uart.int_num = uart->dtd->rx_int;
+	memcpy(&op.uart.cfg, cfg, sizeof(uart_cfg_t));
+	strncpy(op.uart.path, uart->dtd->path, 64);
+	op.uart.path[63] = 0;
+
+	x86_hw_op_write(&op);
+	x86_hw_op_write_writeback(&op);
+
 	return E_OK;
 }
 
@@ -58,7 +102,7 @@ static term_flags_t get_flags(void *cfg){
 }
 
 static char putc(char c, void *data){
-	lnx_write(1, &c, 1);
+	lnx_write(((dev_data_t*)data)->fd, &c, 1);
 
 	return c;
 }
@@ -69,13 +113,11 @@ static size_t putsn(char const *s, size_t n, void *data){
 		return 0;
 	}
 
-	lnx_write(1, s, n);
+	lnx_write(((dev_data_t*)data)->fd, s, n);
 
 	return n;
 }
 
 static size_t gets(char *s, size_t n, term_err_t *err, void *data){
-	lnx_read_fix(0, s, n);
-
-	return n;
+	return lnx_read(((dev_data_t*)data)->fd, s, n);
 }
