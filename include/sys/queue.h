91644d7273955ed39091f3ce770ceb81f45f3ca9
jnow-87 <jan.nowotsch@gmail.com>
2022-05-07 15:27:31 +0200

    [queue: simplify implementation]
    Replace the queue macro implementations with analogous list macros.
    Thus far the queue macros entirely duplicate those list macros.

--- include/sys/queue.h
+++ include/sys/queue.h
@@ -24,11 +24,8 @@
  * \param	tail	tail pointer (must not point reference
  * 					an instance of the target type)
  */
-#define queue_init(head, tail){ \
-	LIST_TYPE_COMPAT(*head); \
-	LIST_TYPE_COMPAT(*tail); \
-	_list1_init((list1_t*)head, (list1_t**)&tail); \
-}
+#define queue_init(head, tail) \
+	list1_init(head, tail)
 
 /**
  * \brief	add an element to the queue
@@ -37,12 +34,8 @@
  * \param	tail	pointer to the queue tail
  * \param	el		pointer to the target element
  */
-#define queue_enqueue(head, tail, el){ \
-	LIST_TYPE_COMPAT(*head); \
-	LIST_TYPE_COMPAT(*tail); \
-	LIST_TYPE_COMPAT(*el); \
-	_list1_add_tail((list1_t**)&head, (list1_t**)&tail, (list1_t*)el); \
-}
+#define queue_enqueue(head, tail, el) \
+	list1_add_tail(head, tail, el)
 
 /**
  * \brief	remove an element from the queue, returning
@@ -54,14 +47,13 @@
  * \return	pointer to the element
  */
 #define queue_dequeue(head, tail)({ \
-	LIST_TYPE_COMPAT(*head); \
-	LIST_TYPE_COMPAT(*tail); \
 	typeof(head) _el; \
+	\
 	\
 	_el = list_first(head); \
 	\
 	if(_el != 0x0) \
-		_list1_rm_head((list1_t**)&head, (list1_t**)&tail); \
+		list1_rm_head(head, tail); \
 	\
 	_el; \
 })
