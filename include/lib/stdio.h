6eb2f318858a4f286591cd343ab349c85d2c580e
jnow-87 <jan.nowotsch@gmail.com>
2022-09-10 13:12:54 +0200

    [test: getopt]
    Add tests for getopt. Remove lib/ from the list of ignored coverage
    directories.

--- include/lib/stdio.h
+++ include/lib/stdio.h
@@ -11,11 +11,15 @@
 #define LIB_STDIO_H
 
 
-#include <config/config.h>
-#include <sys/compiler.h>
-#include <sys/stream.h>
-#include <sys/stdarg.h>
-#include <sys/fcntl.h>
+#ifndef BUILD_HOST
+# include <config/config.h>
+# include <sys/compiler.h>
+# include <sys/stream.h>
+# include <sys/stdarg.h>
+# include <sys/fcntl.h>
+#else // BUILD_HOST
+# include <stdio.h>
+#endif // BUILD_HOST
 
 
 /* global variables */
@@ -25,6 +29,7 @@ extern FILE *stdin,
 
 
 /* prototypes */
+#ifndef BUILD_HOST
 FILE *fopen(char const *path, char const *mode);
 FILE *fdopen(int fd, char const *mode);
 int fileno(FILE *stream);
@@ -46,33 +51,36 @@ int ftell(FILE *stream);
 int printf(char const *format, ...);
 int fprintf(FILE *stream, char const *format, ...);
 int vprintf(char const *format, va_list lst);
+#endif // BUILD_HOST
 
 
 /* disabled-call macros */
-#ifndef CONFIG_SC_FILESYSTEM
-# define fopen(path, mode)				(FILE*)CALL_DISABLED(fopen, CONFIG_SC_FILESYSTEM)
-# define fdopen(fd, mode)				(FILE*)CALL_DISABLED(fdopen, CONFIG_SC_FILESYSTEM)
-# define fclose(stream)					CALL_DISABLED(fclose, CONFIG_SC_FILESYSTEM)
+#ifndef BUILD_HOST
+# ifndef CONFIG_SC_FILESYSTEM
+#  define fopen(path, mode)				(FILE*)CALL_DISABLED(fopen, CONFIG_SC_FILESYSTEM)
+#  define fdopen(fd, mode)				(FILE*)CALL_DISABLED(fdopen, CONFIG_SC_FILESYSTEM)
+#  define fclose(stream)				CALL_DISABLED(fclose, CONFIG_SC_FILESYSTEM)
 
-# define fread(p, size, stream)			CALL_DISABLED(fread, CONFIG_SC_FILESYSTEM)
-# define fwrite(p, size, stream)		CALL_DISABLED(fwrite, CONFIG_SC_FILESYSTEM)
+#  define fread(p, size, stream)		CALL_DISABLED(fread, CONFIG_SC_FILESYSTEM)
+#  define fwrite(p, size, stream)		CALL_DISABLED(fwrite, CONFIG_SC_FILESYSTEM)
 
-# define fgetc(stream)					CALL_DISABLED(fgetc, CONFIG_SC_FILESYSTEM)
-# define fgets(s, size, stream)			CALL_DISABLED(fgets, CONFIG_SC_FILESYSTEM)
+#  define fgetc(stream)					CALL_DISABLED(fgetc, CONFIG_SC_FILESYSTEM)
+#  define fgets(s, size, stream)		CALL_DISABLED(fgets, CONFIG_SC_FILESYSTEM)
 
-# define fputc(c, stream)				CALL_DISABLED(fputc, CONFIG_SC_FILESYSTEM)
-# define fputs(s, stream)				(char*)CALL_DISABLED(fputs, CONFIG_SC_FILESYSTEM)
+#  define fputc(c, stream)				CALL_DISABLED(fputc, CONFIG_SC_FILESYSTEM)
+#  define fputs(s, stream)				(char*)CALL_DISABLED(fputs, CONFIG_SC_FILESYSTEM)
 
-# define fflush(stream)					CALL_DISABLED(fflush, CONFIG_SC_FILESYSTEM)
-# define fseek(stream, offset, whence)	CALL_DISABLED(fseek, CONFIG_SC_FILESYSTEM)
-# define ftell(stream)					CALL_DISABLED(ftell, CONFIG_SC_FILESYSTEM)
-#endif // CONFIG_SC_FILESYSTEM
+#  define fflush(stream)				CALL_DISABLED(fflush, CONFIG_SC_FILESYSTEM)
+#  define fseek(stream, offset, whence)	CALL_DISABLED(fseek, CONFIG_SC_FILESYSTEM)
+#  define ftell(stream)					CALL_DISABLED(ftell, CONFIG_SC_FILESYSTEM)
+# endif // CONFIG_SC_FILESYSTEM
 
-#ifndef CONFIG_LIB_PRINTF
-# define printf(format, ...)			CALL_DISABLED(printf, CONFIG_LIB_PRINTF)
-# define fprintf(stream, format, ...)	CALL_DISABLED(fprintf, CONFIG_LIB_PRINTF)
-# define vprintf(format, lst)			CALL_DISABLED(vprintf, CONFIG_LIB_PRINTF)
-#endif // CONFIG_LIB_PRINTF
+# ifndef CONFIG_LIB_PRINTF
+#  define printf(format, ...)			CALL_DISABLED(printf, CONFIG_LIB_PRINTF)
+#  define fprintf(stream, format, ...)	CALL_DISABLED(fprintf, CONFIG_LIB_PRINTF)
+#  define vprintf(format, lst)			CALL_DISABLED(vprintf, CONFIG_LIB_PRINTF)
+# endif // CONFIG_LIB_PRINTF
+#endif // BUILD_HOST
 
 
 #endif // LIB_STDIO_H
