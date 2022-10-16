920b1f1ea83886a76272885993909c53ce306eb1
jnow-87 <jan.nowotsch@gmail.com>
2022-10-16 18:40:38 +0200

    [avr: restructure]
    Split arch/avr into kernel and lib.

---
+++ arch/avr/kernel/syscall.c
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2022 Jan Nowotsch
+ * Author Jan Nowotsch	<jan.nowotsch@gmail.com>
+ *
+ * Released under the terms of the GNU GPL v2.0
+ */
+
+
+
+#include <kernel/interrupt.h>
+#include <kernel/init.h>
+#include <kernel/syscall.h>
+#include <sys/errno.h>
+#include <sys/syscall.h>
+
+
+/* local/static prototypes */
+static void sc_hdlr(int_num_t num, void *payload);
+
+
+/* local functions */
+static int init(void){
+	return int_register(AVR_NUM_HW_INTS, sc_hdlr, 0x0);
+}
+
+platform_init(0, init);
+
+static void sc_hdlr(int_num_t num, void *payload){
+	sc_t *sc;
+
+
+	sc = (sc_t*)(mreg_r(GPIOR0) | (mreg_r(GPIOR1) << 8));
+
+	sc_khdlr(sc->num, sc->param, sc->size);
+	sc->errno = errno;
+}
